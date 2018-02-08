2f1989116e3b6aaef96d2b54123e31d8fc746a60
Jens Becker <jensbecker@gmail.com>
2018-02-08 21:11:50 +0100

    add readability parser

---
+++ bin/parser/parser.js
@@ -0,0 +1,27 @@
+var req = require('request');
+var jsdom = require("jsdom").jsdom;
+
+var readability = require("readability-node");
+var Readability = readability.Readability;
+
+var uri = process.argv[2];
+
+req.get(uri, function (error, response, body) {
+
+    var doc = jsdom(body, {
+      features: {
+        FetchExternalResources: false,
+        ProcessExternalResources: false
+      }
+    });
+    var article = new Readability({}, doc).parse();
+
+    var json = {
+      'title': article.title,
+      'excerpt': article.excerpt,
+      'author': article.byline,
+      'content': article.content
+    }
+
+    console.log(JSON.stringify(json));
+});
