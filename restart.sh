b26f3b152d585932f62453b1c0b248ee3dc052eb
jlorek <lorek.jens@gmail.com>
2018-02-10 23:11:11 +0100

    updated restart script

--- restart.sh
+++ restart.sh
@@ -1,5 +1,10 @@
 kill -9 `ps -aux | grep '/usr/lib/erlang' | grep -v grep | awk '{print $2}'`
 git pull
 mix deps.get --only prod
+cd assets
+npm install
+brunch build --production
+cd ..
+MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix ecto.migrate
 MIX_ENV=prod elixir --detached -S mix do compile, phx.server
